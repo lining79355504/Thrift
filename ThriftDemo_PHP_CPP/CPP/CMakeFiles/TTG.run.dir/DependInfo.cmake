92ceadd0e675a84a04fad6b603a58c0a5241a865
suoer <suoer@mogujie.com>
2016-07-22 16:23:13 +0800

    asdasd

---
+++ ThriftDemo_PHP_CPP/CPP/CMakeFiles/TTG.run.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/2015-218-pc/github/thrift/ThriftDemo_PHP_CPP/CPP/Server.cpp" "/Users/2015-218-pc/github/thrift/ThriftDemo_PHP_CPP/CPP/CMakeFiles/TTG.run.dir/Server.cpp.o"
+  "/Users/2015-218-pc/github/thrift/ThriftDemo_PHP_CPP/CPP/TTGService.cpp" "/Users/2015-218-pc/github/thrift/ThriftDemo_PHP_CPP/CPP/CMakeFiles/TTG.run.dir/TTGService.cpp.o"
+  "/Users/2015-218-pc/github/thrift/ThriftDemo_PHP_CPP/CPP/TTG_constants.cpp" "/Users/2015-218-pc/github/thrift/ThriftDemo_PHP_CPP/CPP/CMakeFiles/TTG.run.dir/TTG_constants.cpp.o"
+  "/Users/2015-218-pc/github/thrift/ThriftDemo_PHP_CPP/CPP/TTG_types.cpp" "/Users/2015-218-pc/github/thrift/ThriftDemo_PHP_CPP/CPP/CMakeFiles/TTG.run.dir/TTG_types.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ThriftDemo_PHP_CPP/CPP/CMakeFiles/TTG.run.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TTG.run.dir/Server.cpp.o"
+  "CMakeFiles/TTG.run.dir/TTG_constants.cpp.o"
+  "CMakeFiles/TTG.run.dir/TTG_types.cpp.o"
+  "CMakeFiles/TTG.run.dir/TTGService.cpp.o"
+  "TTG.run.pdb"
+  "TTG.run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TTG.run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
